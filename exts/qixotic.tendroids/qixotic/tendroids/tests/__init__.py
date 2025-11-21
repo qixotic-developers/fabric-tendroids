f8bb2a3ac9b1481a4219cd59a3a207dad34621df
qixotic-software <qixoticsoftware@gmail.com>
2025-11-20 22:18:00 -0500

    FABTEND-0034_clean-up-after-waves-experiments

---
+++ exts/qixotic.tendroids/qixotic/tendroids/tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Test modules for Tendroids system
+
+Contains integration tests, stress tests, and performance benchmarks.
+"""
+
+# This directory contains test scripts that should be run from
+# Omniverse Script Editor or standalone Python environment
